302c7e34ac24c2dbdf89a3ab068178b271f7d57c
juhui <juya202106@naver.com>
2025-08-05 14:40:39 +0900

    feat: 로그인 페이지 초기 구현

---
+++ src/app/user/login/page.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Login.css';
+//import mainImage from './images/main-image.png';
+
+export default function Login() {
+    return (
+        <div className="login-root">
+            <div className="login-card">
+                <img src="/header/header-logo.png" alt="main visual" className="login-main-image" />
+                <form className="login-form">
+                    <input
+                        className="login-input"
+                        type="text"
+                        placeholder="아이디를 입력해 주세요."
+                        required
+                    />
+                    <input
+                        className="login-input"
+                        type="password"
+                        placeholder="비밀번호를 입력해 주세요."
+                        required
+                    />
+                    <button className="login-btn" type="submit">
+                        로그인
+                    </button>
+                </form>
+                <div className="login-links">
+                    <a href="#" className="signup-link">계정이 없으신가요? 회원가입</a>
+                    <div className="find-links">
+                        <a href="#" className="find-link">아이디 찾기</a>
+                        <span className="divider">|</span>
+                        <a href="#" className="find-link">비밀번호 찾기</a>
+                    </div>
+                </div>
+                <div className="login-sns">
+                    <button className="sns-btn google">구글 아이디로 로그인</button>
+                    <button className="sns-btn kakao">카카오 아이디로 로그인</button>
+                </div>
+            </div>
+        </div>
+    );
+}
