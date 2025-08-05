033f84d029a568e490ad8c5dbd62d8d3ccfc9124
GIYEOL1117 <bgy11178@gmail.com>
2025-08-05 11:52:22 +0900

    docs: 이미지파일에서 url로 수정

--- src/app/review/components/review-service-main/My-Review-MainPage.jsx
+++ src/app/review/components/review-service-main/My-Review-MainPage.jsx
@@ -6,10 +6,10 @@ import Sidebar from '@/components/common/Sidebar';
 import { Button } from '@/components/ui/button';
 
 const mockReviews = [
-    { id: 1, title: '아가 까까 팜', rating: 5, date: '2014년 02월 02일', image: '/images/review/cookie.webp' },
-    { id: 2, title: '분유 사실분!~', rating: 5, date: '2020년 10월 30일', image: '/images/review/baby.jpg' },
-    { id: 3, title: '오징어 인형 팔아요', rating: 5, date: '2023년 02월 11일', image: '/images/review/fish.webp' },
-    { id: 4, title: '버섯 장난감 판매합니다', rating: 5, date: '2003년 03월 02일', image: '/images/review/mush.jpeg' },
+    { id: 1, title: '아가 까까 팜', rating: 5, date: '2014년 02월 02일', image: 'https://i.namu.wiki/i/Hv0V4WWCm_FEi9CgeCx6B59r4WXsbx8rw42vpmwtge33R0d5qOrmU9Ys8ly7aEuCs7yKRz4QaQk53vL1ZoXO4w.webp' },
+    { id: 2, title: '분유 사실분!~', rating: 5, date: '2020년 10월 30일', image: 'https://direct.maeil.com/UploadedFiles/direct/product/16384cfd-f0df-41d0-ba74-adeb175f2538.jpg' },
+    { id: 3, title: '오징어 인형 팔아요', rating: 5, date: '2023년 02월 11일', image: 'https://i.namu.wiki/i/CJ26nMlkCYi6VK26GJW7fQ6Eh9DjuYXMK4vZXYx8CQIElySFfd8AU_MbFxkCkoK23uFv7DZ3Wwa3qNePlaCkRQ.webp' },
+    { id: 4, title: '버섯 장난감 판매합니다', rating: 5, date: '2003년 03월 02일', image: 'https://cdn.psnews.co.kr/news/photo/202404/2051984_101923_2627.jpg' },
 ];
 
 const MyReviewMainPage = () => {
