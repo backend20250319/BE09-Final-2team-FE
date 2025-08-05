c5401b337e3a3c22a471c75a17fdbb91747ac4f0
GIYEOL1117 <bgy11178@gmail.com>
2025-08-05 11:40:23 +0900

    사이드바 생성

--- src/app/review/page.jsx
+++ src/app/review/page.jsx
@@ -1,5 +1,8 @@
-"use client";
+'use client';
+
+import React from 'react';
+import MyReviewMainPage from './components/review-service-main/My-Review-MainPage';
 
 export default function Page() {
-  return <>page</>;
+  return <MyReviewMainPage />;
 }
