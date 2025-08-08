9b476e3a88b8be2032de38cdeb2ed12d9d4ccc80
jungho <kiik52@naver.com>
2025-08-08 12:20:44 +0900

    fix: 404 페이지 이미지 변경

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center h-full gap-2 pt-[50px]">
-      <Image src={"/images/404-page.png"} width={440} height={440} alt="404-not-found" />
+      <Image src={"/images/404-page.png"} width={350} height={350} alt="404-not-found" />
       <Button className="h-[50px]">
         <span className="text-lg">Momnect 홈으로 돌아가기</span>
       </Button>
