c0551a0d3131c8b12dcf8af777f7afa6846171ef
jungho <kiik52@naver.com>
2025-08-08 12:03:25 +0900

    add: 404 에러페이지 추가 및 로고 이미지 변경

---
+++ src/app/not-found.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Info } from "lucide-react";
+import Image from "next/image";
+
+// app/not-found.jsx
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center h-full gap-2 pt-[50px]">
+      <Image src={"/images/404-page.png"} width={440} height={440} alt="404-not-found" />
+      <Button className="h-[50px]">
+        <span className="text-lg">Momnect 홈으로 돌아가기</span>
+      </Button>
+    </div>
+  );
+}
