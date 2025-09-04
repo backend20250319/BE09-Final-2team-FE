3a937372313685bbc4105f4453e25cd07d4a9ba4
jungho <kiik52@naver.com>
2025-09-04 12:35:23 +0900

    modify: aws 인증 권한 추가

--- src/app/chat/components/test.jsx
+++ src/app/chat/components/test.jsx
@@ -8,6 +8,7 @@ export default function Test() {
       <p>배포 테스트 2</p>
       <p>배포 테스트 3</p>
       <p>Elastic Beanstalk 어플리케이션 및 환경 설정 재구성</p>
+      <p>EC2 인스턴스 프로파일(aws-elasticbeanstalk-ec2-role)에 AmazonEC2ContainerRegistryReadOnly 권한 추가</p>
     </div>
   );
 }
